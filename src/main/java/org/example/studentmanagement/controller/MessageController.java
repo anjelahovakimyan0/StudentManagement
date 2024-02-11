d89c184910df41189fa4fb8b1258b64055fc7d81
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2024-02-11 22:54:41 +0300

    homework

--- src/main/java/org/example/studentmanagement/controller/MessageController.java
+++ src/main/java/org/example/studentmanagement/controller/MessageController.java
@@ -7,7 +7,11 @@ import org.example.studentmanagement.service.MessageService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
